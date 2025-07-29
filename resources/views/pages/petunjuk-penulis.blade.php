a7dace09b7b63863fe5b8a2a2b96f1be9c487612
iam-allend <contact.anurmustakim@gmail.com>
2025-07-29 15:36:47 +0700

    update page petunjuk penulisan, ircs journal

--- resources/views/pages/petunjuk-penulis.blade.php
+++ resources/views/pages/petunjuk-penulis.blade.php
@@ -19,34 +19,34 @@
 
         <div class="container-fluid py-5">
             <div class="container pb-5">
-                <div class="text-center m-auto pb-2" style="max-width: 900px;">
+                <div class="text-center m-auto pb-2 wow fadeInDown" data-wow-delay="0.2s" style="max-width: 900px;">
                     <!-- <h4 class="text-primary">Menerbitkan Buku</h4> -->
                     <h1 class="display-6">Menerbitkan Buku Bersama Ritecs</h1>
-                    <p class="btn btn-dark rounded-pill px-4 small text-nowrap fw-bold">Jenis buku yang kami terbitkan</p>
+                    <p class="btn text-white bg-dark rounded-pill px-4 py-2 small text-nowrap fw-bold">Jenis buku yang kami terbitkan</p>
                 </div>
                 <div class="row justify-content-center">
-                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0">
+                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0 wow fadeInDown" data-wow-delay="0.3s">
                         <a href="#" class="btn btn-outline-dark fw-light rounded-pill px-4 small w-100 text-nowrap">Buku Ajar</a>
                     </div>
-                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0">
+                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0 wow fadeInDown" data-wow-delay="0.4s">
                         <a href="#" class="btn btn-outline-dark fw-light rounded-pill px-4 small w-100 text-nowrap">Monograf</a>
                     </div>
-                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0">
+                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0 wow fadeInDown" data-wow-delay="0.5s">
                         <a href="#" class="btn btn-outline-dark fw-light rounded-pill px-4 small w-100 text-nowrap">Buku Referensi</a>
                     </div>
-                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0">
+                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0 wow fadeInDown" data-wow-delay="0.7s">
                         <a href="#" class="btn btn-outline-dark fw-light rounded-pill px-4 small w-100 text-nowrap">Buku Populer Ilmiah</a>
                     </div>
-                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0">
+                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0 wow fadeInDown" data-wow-delay="0.8s">
                         <a href="#" class="btn btn-outline-dark fw-light rounded-pill px-4 small w-100 text-nowrap">Buku Modul / Praktikum</a>
                     </div>
-                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0">
+                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0 wow fadeInDown" data-wow-delay="0.9s">
                         <a href="#" class="btn btn-outline-dark fw-light rounded-pill px-4 small w-100 text-nowrap">Buku Konferensi / Prosiding</a>
                     </div>
-                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0">
+                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0 wow fadeInDown" data-wow-delay="1s">
                         <a href="#" class="btn btn-outline-dark fw-light rounded-pill px-4 small w-100 text-nowrap">Buku Non-Fiksi Umum</a>
                     </div>
-                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0">
+                    <div class="col col-12 col-md-6 col-xl-4 col-xxl-3 p-2 m-0 wow fadeInDown" data-wow-delay="1.1s">
                         <a href="#" class="btn btn-outline-dark fw-light rounded-pill px-4 small w-100 text-nowrap">Buku Fiksi</a>
                     </div>
                 </div>
@@ -63,21 +63,21 @@
             </div>
 
             <div class="container px-md-5 px-1 text-center" style="max-width: 1000px;">
-                <div class="item-skema pb-3 pt-0 wow fadeInUp" data-wow-delay="0.4s">
+                <div class="item-skema pb-3 pt-0 wow fadeInUp" data-wow-delay="0.3s">
                     <h5 class="m-0 p-0 my-2">Buku Elektronik ber-eISBN dengan Akses Terbuka</h5>
                     <p class="p-0 m-0">
                         Akses terbuka (open access) adalah model publikasi yang memungkinkan siapa pun untuk mengakses, membaca, dan menggunakan karya ilmiah secara gratis melalui internet, tanpa perlu membayar atau berlangganan. Dalam sistem ini, pengetahuan dapat disebarluaskan lebih luas dan lebih cepat, mendukung kolaborasi, transparansi, serta kemajuan ilmu pengetahuan. Oleh karenanya, kami akan mendukung penulis dalam melakukan distribusi untuk dapat diakses dengan jangkauan lebih luas sehingga dapat meningkatkan jumlah akses pembaca. 
                     </p>
                 </div>
 
-                <div class="item-skema py-3 wow fadeInUp" data-wow-delay="0.6s">
+                <div class="item-skema py-3 wow fadeInUp" data-wow-delay="0.4s">
                     <h5 class="m-0 p-0 my-2">Buku Elektronik ber-eISBN dengan Akses Tertutup</h5>
                     <p class="p-0 m-0">
                         Akses tertutup (closed-access) adalah model publikasi di mana karya ilmiah hanya dapat diakses oleh pembaca yang membayar atau memiliki langganan. Dalam sistem ini, artikel atau buku tidak tersedia secara bebas untuk umum, melainkan dibatasi oleh paywall atau sistem keanggotaan. Akses tertutup membatasi jangkauan pembaca dan umumnya tidak mengizinkan penggunaan ulang tanpa izin khusus, sehingga penyebaran ilmu pengetahuan menjadi lebih terbatas.  Dalam hal ini, strategi distribusi dan penjualan akan didiskusikan antara Ritecs dan penulis untuk mendapatkan kesepakatan. 
                     </p>
                 </div>
 
-                <div class="item-skema py-3 wow fadeInUp" data-wow-delay="0.8s">
+                <div class="item-skema py-3 wow fadeInUp" data-wow-delay="0.5s">
                     <h5 class="m-0 p-0 my-2">Buku Cetak ber-ISBN</h5>
                     <p class="p-0 m-0">
                         Skema ini berlaku untuk karya ilmiah yang hanya dapat diakses oleh pembaca yang membeli buku. Dalam hal ini, penulis diwajibkan untuk menanggung biaya cetak untuk 100-500 eksemplar buku. Strategi distribusi dan penjualan akan didiskusikan antara Ritecs dan penulis untuk mendapatkan kesepakatan.
