0e52eb1da81b1ec0ba9f99065094e80496038a58
syarif32 <najwasyarif563@g,ail.com>
2025-07-30 13:58:16 +0700

    ambil update

--- resources/views/pages/buku.blade.php
+++ resources/views/pages/buku.blade.php
@@ -2,25 +2,105 @@
 
 @section('content')
 
-    <div class="container-fluid feature bg-light py-5">
+    <div class="container-fluid feature bg-white py-5">
 
         <!-- Buku Start -->
-        <div class="container-fluid blog py-5 bg-white">
-            <div class="container py-5">
+        <div class="container-fluid blog py-5">
+            <div class="container py-5 text-center">
                 <div class="text-center mx-auto pb-5 wow fadeInUp" data-wow-delay="0.2s" style="max-width: 800px;">
                     <h4 class="text-primary">Blog</h4>
                     <h1 class="display-4 mb-4">News And Updates</h1>
-                    <h3 class="display-4 mb-4">Book & Journal</h1>
-                        <p class="mb-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur adipisci facilis
-                            cupiditate recusandae aperiam temporibus corporis itaque quis facere, numquam, ad culpa deserunt
-                            sint dolorem autem obcaecati, ipsam mollitia hic.
-                        </p>
+                    <!-- <h3 class="display-4 mb-4">Book & Journal</h1> -->
+                    <p class="mb-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur adipisci facilis cupiditate recusandae aperiam temporibus corporis itaque quis facere, numquam, ad culpa deserunt sint dolorem autem obcaecati, ipsam mollitia hic.
+                    </p>
                 </div>
-                
+                <div class="row g-4 justify-content-center text-start">
+
+                    <div class="col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.6s">
+                        <div class="blog-item">
+                            <div class="blog-img rounded-top">
+                                <img src="assets/img/book1.png" class="img-fluid rounded-top w-100" alt="">
+                                <div class="blog-categiry py-2 px-4">
+                                    <span>Buku</span>
+                                </div>
+                            </div>
+                            <div class="blog-content p-4">
+                                <div class="blog-comment d-flex justify-content-between mb-3">
+                                    <div class="small"><span class="fa fa-user text-primary"></span> Martin.C</div>
+                                    <div class="small"><span class="fa fa-calendar text-primary"></span> 30 Dec 2025</div>
+                                   
+                                </div>
+                                <a href="#" class="h5 d-inline-block mb-3">Leverage agile frameworks to provide</a>
+                                <p class="mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius libero soluta impedit.   </p>
+                                <a href="#" class="p-0">Read More  <i class="fa fa-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.4s">
+                        <div class="blog-item ">
+                            <div class="blog-img rounded-top">
+                                <img src="assets/img/jurnal.png" class="img-fluid rounded-top w-100" alt="">
+                                <div class="blog-categiry py-2 px-4">
+                                    <span>Jurnal</span>
+                                </div>
+                            </div>
+                            <div class="blog-content p-4">
+                                <div class="blog-comment d-flex justify-content-between mb-3">
+                                    <div class="small"><span class="fa fa-user text-primary"></span> Radit R.</div>
+                                    <div class="small"><span class="fa fa-calendar text-primary"></span> 30 Dec 2025</div>
+                                    
+                                </div>
+                                <a href="#" class="h5 d-inline-block mb-3">Leverage agile frameworks to provide</a>
+                                <p class="mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius libero soluta impedit.   </p>
+                                <a href="#" class="p-0">Read More  <i class="fa fa-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.6s">
+                        <div class="blog-item">
+                            <div class="blog-img rounded-top">
+                                <img src="assets/img/buku1.png" class="img-fluid rounded-top w-100" alt="">
+                                <div class="blog-categiry py-2 px-4">
+                                    <span>Buku</span>
+                                </div>
+                            </div>
+                            <div class="blog-content p-4">
+                                <div class="blog-comment d-flex justify-content-between mb-3">
+                                    <div class="small"><span class="fa fa-user text-primary"></span> Martin.C</div>
+                                    <div class="small"><span class="fa fa-calendar text-primary"></span> 30 Dec 2025</div>
+                                   
+                                </div>
+                                <a href="#" class="h5 d-inline-block mb-3">Leverage agile frameworks to provide</a>
+                                <p class="mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius libero soluta impedit.   </p>
+                                <a href="#" class="p-0">Read More  <i class="fa fa-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.6s">
+                        <div class="blog-item">
+                            <div class="blog-img rounded-top">
+                                <img src="assets/img/buku1.png" class="img-fluid rounded-top w-100" alt="">
+                                <div class="blog-categiry py-2 px-4">
+                                    <span>Buku</span>
+                                </div>
+                            </div>
+                            <div class="blog-content p-4">
+                                <div class="blog-comment d-flex justify-content-between mb-3">
+                                    <div class="small"><span class="fa fa-user text-primary"></span> Martin.C</div>
+                                    <div class="small"><span class="fa fa-calendar text-primary"></span> 30 Dec 2025</div>
+                                   
+                                </div>
+                                <a href="#" class="h5 d-inline-block mb-3">Leverage agile frameworks to provide</a>
+                                <p class="mb-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius libero soluta impedit.   </p>
+                                <a href="#" class="p-0">Read More  <i class="fa fa-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <a href="" class="btn btn-primary rounded-pill py-2 px-5 mt-4 m-auto text-center wow fadeInUp" data-wow-delay="0.8s" >Berita Lainnya</a>
             </div>
         </div>
-        <!-- Buku End -->
-
     </div>
 
 @endsection
